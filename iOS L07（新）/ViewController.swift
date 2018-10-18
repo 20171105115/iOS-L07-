2f938e15a55c2a8bf9a8cfdc1c577e0d16dd5753
朱博宇 <s20171105115@chu1e1adeiMac.local>
2018-10-18 09:51:02 +0800

    ios 07(computer)
    尝试使用堆栈，但是swift中很多C语言中的语法使用不了，只能慢慢尝试。

--- iOS L07（新）/ViewController.swift
+++ iOS L07（新）/ViewController.swift
@@ -8,8 +8,54 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
+    struct stack
+    {
+        var a = [Int] ()
+        var top = -1
+    }
+    var SomeStack = stack ()
+    struct nstack
+    {
+        var a = [String] ()
+        var ntop = -1
+    }
+    var SomeNstack = nstack ()
+    func npush(ch:String){
+        SomeNstack.ntop += 1
+        SomeNstack.a[SomeNstack.ntop] = ch
+    }
+    func push(ch:Int){
+        SomeStack.top += 1
+        SomeStack.a[SomeStack.top] = ch
+    }
+    func pop(ns:nstack)->String{
+        return ns.a[ns.ntop]
+    }
+    func npop(s:nstack)->String{
+        return s.a[s.ntop]
+    }
+    func isnumber(ch:String)->Bool{
+        if "0"<=ch&&ch<="9" {
+            return true
+        }else{
+            return false
+        }
+    }
+    func iscmpty(ns:nstack)->Bool{
+        if ns.ntop == -1{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
